aea91803813b65d84b7c837aa20eafd6761f020c
yokoro13 <yokoro1219@yahoo.co.jp>
2020-02-14 17:07:19 +0900

    ADD チャット機能

--- src/OekakiApplication.kt
+++ src/OekakiApplication.kt
@@ -1,13 +1,85 @@
 package com.yokoro
 
+import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.features.*
+import io.ktor.http.cio.websocket.*
+import io.ktor.http.content.defaultResource
+import io.ktor.http.content.resources
+import io.ktor.http.content.static
+import io.ktor.jackson.jackson
+import io.ktor.routing.routing
+import io.ktor.sessions.*
+import io.ktor.util.KtorExperimentalAPI
+import io.ktor.util.generateNonce
+import io.ktor.websocket.WebSockets
+import io.ktor.websocket.webSocket
+import kotlinx.coroutines.channels.consumeEach
+import java.time.Duration
+
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.main(testing: Boolean = false) {
+    OekakiApplication().apply { main() }
 }
 
+class OekakiApplication {
+    val server = OekakiServer()
+
+    @KtorExperimentalAPI
+    fun Application.main(){
+        install(DefaultHeaders)
+        install(CallLogging)
+        install(WebSockets){
+            pingPeriod = Duration.ofMillis(1)
+        }
+        install(Sessions){
+            cookie<OekakiSession>("SESSION")
+        }
+        install(ContentNegotiation){
+            jackson {
+                configure(SerializationFeature.INDENT_OUTPUT, true)
+            }
+        }
+        intercept(ApplicationCallPipeline.Features){
+            if(call.sessions.get<OekakiSession>() == null){
+                call.sessions.set(OekakiSession(generateNonce()))
+            }
+        }
+
+        routing() {
+            webSocket("/ws") {
+                val session = call.sessions.get<OekakiSession>()
+
+                if(session == null){
+                    close(CloseReason(CloseReason.Codes.VIOLATED_POLICY, "No session"))
+                    return@webSocket
+                }
+
+                server.memberJoin(session.id, this)
+
+                try {
+                    incoming.consumeEach { frame ->
+                        if(frame is Frame.Text){
+                            server.message(session.id, frame.readText())
+                        }
+                    }
+                } finally {
+                    server.memberLeft(session.id, this)
+                }
+
+                static {
+                    defaultResource("index.html", "web")
+                    resources("web")
+                }
+
+            }
+        }
+    }
+
+    data class OekakiSession(val id: String)
+
+}
--- src/OekakiServer.kt
+++ src/OekakiServer.kt
@@ -1,2 +1,96 @@
 package com.yokoro
 
+import io.ktor.http.cio.websocket.CloseReason
+import io.ktor.http.cio.websocket.Frame
+import io.ktor.http.cio.websocket.WebSocketSession
+import io.ktor.http.cio.websocket.close
+import kotlinx.coroutines.channels.ClosedSendChannelException
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.CopyOnWriteArrayList
+import java.util.concurrent.atomic.AtomicInteger
+
+class OekakiServer (){
+
+    val userCounter = AtomicInteger()
+
+    val memberNames = ConcurrentHashMap<String, String>()
+
+    val members = ConcurrentHashMap<String, MutableList<WebSocketSession>>()
+
+    val lastMessages = LinkedList<String>()
+
+    suspend fun memberJoin(member: String, socket: WebSocketSession){
+        val name = memberNames.computeIfAbsent(member) {"user${userCounter.incrementAndGet()}"}
+
+        val list = members.computeIfAbsent(member) {CopyOnWriteArrayList<WebSocketSession>()}
+        list.add(socket)
+
+        if(list.size == 1){
+            broadcast("server", "Member joined: $name.")
+        }
+
+        val messages = synchronized(lastMessages) {lastMessages.toList()}
+        for (message in messages){
+            socket.send(Frame.Text(message))
+        }
+    }
+
+    suspend fun memberLeft(member: String, socket: WebSocketSession){
+        val connections = members[member]
+        connections?.remove(socket)
+
+        if(connections != null && connections.isEmpty()){
+            val name = memberNames.remove(member) ?: member
+             broadcast("server", "Member left: $name.")
+        }
+    }
+
+    suspend fun memberRenamed(member: String, to: String){
+        val oldName = memberNames.put(member, to) ?: member
+        broadcast("server", "Member renamed from $oldName to $to")
+    }
+
+    suspend fun sendTo(recipient: String, sender: String, message: String){
+        members[recipient]?.send(Frame.Text("[$sender] $message"))
+    }
+
+    suspend fun message(sender: String, message: String){
+        val name = memberNames[sender] ?: sender
+        val formatted = "[$name] $message"
+
+        broadcast(formatted)
+
+        synchronized(lastMessages) {
+            lastMessages.add(formatted)
+            if(lastMessages.size > 100){
+                lastMessages.removeFirst()
+            }
+        }
+    }
+
+    private suspend fun broadcast(message: String){
+        members.values.forEach { socket ->
+            socket.send(Frame.Text(message))
+        }
+    }
+
+    private suspend fun broadcast(sender: String, message: String){
+        val name = memberNames[sender] ?: sender
+        broadcast("[$name] $message")
+    }
+
+    suspend fun List<WebSocketSession>.send(frame: Frame){
+        forEach {
+            try {
+                it.send(frame.copy())
+            } catch (t: Throwable){
+                try {
+                    it.close(CloseReason(CloseReason.Codes.PROTOCOL_ERROR, ""))
+                } catch (ignore: ClosedSendChannelException){
+
+                }
+            }
+        }
+    }
+}
